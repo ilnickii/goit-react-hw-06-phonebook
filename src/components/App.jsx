a04a99db010f37827b50afe364a58dd215dd5a94
ilnickii <ilnickii2008@gmail.com>
2023-12-03 22:16:08 +0300

    added phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactsList } from './ContactList/Contactlist';
+import { Filter } from './Filter/Filter';
+import css from './App.module.css';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.container}>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactsList />
     </div>
   );
 };
